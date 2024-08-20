6e86726b18085e69d77f49e68390061ffe1ef4fe
aaminha <dks4857@gmail.com>
2024-08-20 21:11:08 +0900

    feat: 헤더 반응형 디자인 적용

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,11 @@
-import { LogoWhite } from '@public/svg'
+'use client'
 
-const HeaderContent = [
+import { useState } from 'react'
+
+import { LogoWhite, MenuIcon } from '@public/svg'
+import { useWindowWidth } from '@src/hooks/useWindowWidth'
+
+const NavContent = [
   { title: 'About Me' },
   { title: 'Archiving' },
   { title: 'Skills' },
@@ -8,23 +13,46 @@ const HeaderContent = [
 ]
 
 export const Header = () => {
+  const [isOpenMobileMenu, setIsOpenMobileMenu] = useState(false)
+  const currentWidth = useWindowWidth()
+
   return (
-    <header className="bg-shadow-primary">
-      <div className="mx-auto flex max-w-7xl items-center justify-between px-14 py-5">
+    <header className="fixed w-full bg-shadow-primary">
+      <div className="mx-auto flex max-w-7xl items-center justify-between px-14 py-4">
         {/* TODO: 로고 클릭 시 스크롤 최상단으로 이동 */}
         <LogoWhite className="cursor-pointer" />
         <nav className="">
-          <ul className="header-4 flex gap-x-10 text-white">
+          <ul className="header-4 xs:hidden flex gap-x-10 text-white sm:hidden md:hidden lg:hidden">
             {/* TODO: 스크롤 위치에 따라 메뉴 활성화 */}
             {/* TODO: 메뉴 클릭 시 해당 섹션으로 스크롤 이동 */}
-            {HeaderContent.map((content) => (
-              <li key={content.title} className="cursor-pointer">
+            {NavContent.map((content) => (
+              <li key={content.title} className="cursor-pointer hover:text-secondary-300">
+                {content.title}
+              </li>
+            ))}
+          </ul>
+          <MenuIcon
+            className="3xl:hidden cursor-pointer xl:hidden 2xl:hidden"
+            onClick={() => setIsOpenMobileMenu((prev) => !prev)}
+          />
+        </nav>
+      </div>
+      {isOpenMobileMenu && currentWidth <= 960 && (
+        <nav className="mx-14 py-2">
+          <ul className="header-4 text-white">
+            {/* TODO: 스크롤 위치에 따라 메뉴 활성화 */}
+            {/* TODO: 메뉴 클릭 시 해당 섹션으로 스크롤 이동 + 메뉴바 닫히도록 */}
+            {NavContent.map((content) => (
+              <li
+                key={content.title}
+                className="cursor-pointer px-12 py-6 hover:text-secondary-300"
+              >
                 {content.title}
               </li>
             ))}
           </ul>
         </nav>
-      </div>
+      )}
     </header>
   )
 }
