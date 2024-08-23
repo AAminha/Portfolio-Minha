ca67ada21c9d47a43cb69bb037f44b23a03a81de
aaminha <dks4857@gmail.com>
2024-08-23 22:34:19 +0900

    feat: Archiving 섹션 구현

--- public/svg.ts
+++ public/svg.ts
@@ -1,5 +1,8 @@
+import ArrowRightIcon from './icons/arrow-right.svg'
+import GithubIcon from './icons/github.svg'
 import MenuIcon from './icons/menu.svg'
+import VelogIcon from './icons/velog.svg'
 import LogoDark from './logo/logo-dark.svg'
 import LogoWhite from './logo/logo-white.svg'
 
-export { LogoDark, LogoWhite, MenuIcon }
+export { ArrowRightIcon, GithubIcon, LogoDark, LogoWhite, MenuIcon, VelogIcon }
