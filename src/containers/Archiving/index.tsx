25a744bd76fb10ef9d292a2f46fc0f19a748c552
aaminha <dks4857@gmail.com>
2024-08-24 19:57:53 +0900

    design: 섹션 타이틀 크기 반응형 적용

--- src/containers/Archiving/index.tsx
+++ src/containers/Archiving/index.tsx
@@ -46,7 +46,7 @@ export const Archiving = () => {
       ref={sectionRef.archiving}
       className="flex flex-col items-center bg-secondary-300 px-[30px] py-[120px]"
     >
-      <h1 className="title-3 mb-[70px] text-primary-500">Archiving</h1>
+      <h1 className="sm:title-4 xs:title-4 title-3 mb-[70px] text-primary-500">Archiving</h1>
       <div className="flex w-full max-w-[936px] flex-wrap items-center justify-between gap-[30px] sm:justify-center md:justify-center lg:justify-center xs:justify-center">
         {ARCHIVING_INFORMATION.map((info) => (
           <Card key={info.title} information={info} />
